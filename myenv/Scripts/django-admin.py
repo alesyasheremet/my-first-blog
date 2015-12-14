982982a1a74c1bd4f628fdb178887076520f34e8
asheremet <alesya.sheremet@gmail.com>
2015-12-14 10:56:16 +0100

    My Django Girls app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\asherem84\appdata\local\programs\djangogirls\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
